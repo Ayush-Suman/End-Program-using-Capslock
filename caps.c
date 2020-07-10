d7cfcd92f15e01e72afc6997fb240d536b7c4e68
Ayush Suman <ayush231suman@gmail.com>
2020-07-10 06:47:59 +0530

    Code Modularity

--- caps.c
+++ caps.c
@@ -2,15 +2,32 @@
 #include<fcntl.h>
 #include<unistd.h>
 
+void capslockPressedHandler(char state);
+void capslockListener(int fd, void (*handler)(char state));
 int main(){
 int caps;
 caps = open("/sys/class/leds/input3::capslock/brightness", O_RDONLY);
-char c;
-int r = pread(caps, &c, 1, 0);
-char s=c;
-while(c==s){
-r = pread(caps, &c, 1, 0);
-}
-printf("%c\n", c);
+capslockListener(caps, capslockPressedHandler);
 close(caps);
 }
+
+void capslockPressedHandler(char state){
+	if(state=='0'){	
+		printf("CapsLock turned OFF\n");
+	}else if(state == '1'){
+		printf("Capslock turned ON\n");
+	} else {
+		printf("WTF?");
+	}
+}
+
+void capslockListener(int fd, void (*handler)(char state)){
+	char c;
+	int r = pread(fd, &c, 1, 0);
+	char s=c;
+	while(c==s){
+		r = pread(fd, &c, 1, 0);
+	}
+	handler(c);
+}
+
